760877c703d5e7cabbd3dde8dee54e0ea63c4ca0
Majid Mokhtari <majidmokht@gmail.com>
2019-01-11 15:28:51 -0800

    node test codes

--- notes.js
+++ notes.js
@@ -1,8 +1,7 @@
-const fib = (n) => {
-    if(n < 2) return n
-    return fib(n-1) + fib(n-2)
-}
+const fib = n => {
+  if (n < 2) return n;
+  return fib(n - 1) + fib(n - 2);
+};
 
-
-module.exports.age = 3
-module.exports.slowFib = fib
+module.exports.age = 3;
+module.exports.slowFib = fib;
