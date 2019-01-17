1ce59a6fd7b15461e7d5b50d7c23b5e0ffd116a2
Majid Mokhtari <majidmokht@gmail.com>
2019-01-16 19:04:23 -0800

    geocode and weather get api

--- app.js
+++
@@ -1,6 +0,0 @@
-const yargs = require('yargs');
-
-const { argv } = yargs;
-const { name } = argv;
-
-console.log('here', name);
--- notes.js
+++
@@ -1,7 +0,0 @@
-const fib = n => {
-  if (n < 2) return n;
-  return fib(n - 1) + fib(n - 2);
-};
-
-module.exports.age = 3;
-module.exports.slowFib = fib;
