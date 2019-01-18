d88e5374f8ea3c3c25479df7ce8e47e449969a5a
Majid Mokhtari <majidmokht@gmail.com>
2019-01-18 14:21:43 -0800

    add weather and location on load

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-//const app = require('./src/app');
-
 const express = require('express');
 const fs = require('fs');
+const weatherApp = require('./src/weatherApp');
 
 const port = process.env.PORT || 8000;
 
@@ -20,10 +19,9 @@ app.use((req, res, next) => {
 });
 
 app.get('/', (req, res) => {
-  res.send({
-    name: 'Andrew',
-    likes: ['Biking', 'Cities']
-  });
+  const address = weatherApp.address;
+  const weather = weatherApp.weather;
+  res.send(`Your current address is: <h2>${address}</h2>${weather}`);
 });
 
 app.get('/user/:id', (req, res) => {
--- src/weatherApp.js
+++ src/weatherApp.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 const argv = yargs
   .options({
     a: {
-      demand: true,
+      demand: false,
       alias: 'address',
       describe: 'Address to fetch weather for',
       string: true
@@ -13,7 +13,8 @@ const argv = yargs
   .help()
   .alias('help', 'h').argv;
 
-var encodedAddress = encodeURIComponent(argv.address);
+const address = '150 Font blvd San Francisco, 94132';
+var encodedAddress = encodeURIComponent(address);
 var geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=AIzaSyDh5HKm75BZXkIKo0GKdtgEnT_NaT_biso`;
 
 axios
@@ -24,13 +25,15 @@ axios
     } else var lat = response.data.results[0].geometry.location.lat;
     var lng = response.data.results[0].geometry.location.lng;
     var weatherUrl = `https://api.darksky.net/forecast/e3a138bee5c40adc7b1513fa6206692e/${lat},${lng}`;
-    console.log(response.data.results[0].formatted_address);
+    const formattedAddress = response.data.results[0].formatted_address;
+    module.exports.address = formattedAddress;
     return axios.get(weatherUrl);
   })
   .then(response => {
     var temperature = response.data.currently.temperature;
     var apparentTemperature = response.data.currently.apparentTemperature;
-    console.log(`It's currently ${temperature}. It feels like ${apparentTemperature}.`);
+    const weather = `Your current temperature is: <h2>${temperature} and it feels like ${apparentTemperature}.</h2>`;
+    module.exports.weather = weather;
   })
   .catch(e => {
     if (e.code === 'ENOTFOUND') {
