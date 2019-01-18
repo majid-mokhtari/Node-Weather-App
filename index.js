477cde23cf5fd55890fd58ec5550227de4ae4b5f
Majid Mokhtari <majidmokht@gmail.com>
2019-01-18 12:54:53 -0800

    add web server and heroku  settings

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-const app = require('./src/app');
+//const app = require('./src/app');
+
+const express = require('express');
+const fs = require('fs');
+
+const port = process.env.PORT || 8000;
+
+var app = express();
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  const log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', err => {
+    if (err) {
+      console.log(err);
+    }
+  });
+  next();
+});
+
+app.get('/', (req, res) => {
+  res.send({
+    name: 'Andrew',
+    likes: ['Biking', 'Cities']
+  });
+});
+
+app.get('/user/:id', (req, res) => {
+  res.send('User: ' + req.params.id + ' ' + req.query.name);
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to handle request'
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
+});
