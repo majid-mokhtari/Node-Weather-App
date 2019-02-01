7860daeec27fc6feed3ca7fc187c4d45620b74d3
Majid Mokhtari <majidmokht@gmail.com>
2019-02-01 14:33:02 -0800

    add jwt auth

--- server/server.js
+++ server/server.js
@@ -33,6 +33,23 @@ app.get('/todos', (req, res) => {
   )
 })
 
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password'])
+  var user = new User(body)
+
+  user
+    .save()
+    .then(() => {
+      return user.generateAuthToken()
+    })
+    .then(token => {
+      res.header('x-auth', token).send(user)
+    })
+    .catch(e => {
+      res.status(400).send(e)
+    })
+})
+
 app.get('/todos/:id', (req, res) => {
   var id = req.params.id
 
@@ -68,24 +85,9 @@ app.post('/todos', (req, res) => {
   )
 })
 
-app.post('/users', (req, res) => {
-  var body = _.pick(req.body, ['email', 'password'])
-  var user = new User(body)
-
-  user
-    .save()
-    .then(user => {
-      res.send(user)
-    })
-    .catch(e => {
-      res.status(400).send(e)
-    })
-})
-
 app.patch('/todos/:id', (req, res) => {
   var id = req.params.id
   var body = _.pick(req.body, ['text', 'completed'])
-
   if (!ObjectID.isValid(id)) {
     return res.status(404).send()
   }
