195af75eb6f5559c7f622e35948d68c0774387bd
Majid Mokhtari <majidmokht@gmail.com>
2019-02-02 16:56:23 -0800

    middleware authentication

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ const { mongoose } = require('./db/mongoose')
 const weatherApp = require('../src/weatherApp')
 const { Todo } = require('./models/todo')
 const { User } = require('./models/user')
+var { authenticate } = require('./middleware/authenticate')
 
 const port = process.env.PORT
 
@@ -22,15 +23,9 @@ app.get('/weather', (req, res) => {
   res.send(`Your current address is: ${address} and ${weather}`)
 })
 
-app.get('/todos', (req, res) => {
-  Todo.find().then(
-    todos => {
-      res.send({ todos })
-    },
-    e => {
-      res.status(400).send(e)
-    }
-  )
+// Users routes
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user)
 })
 
 app.post('/users', (req, res) => {
@@ -48,6 +43,18 @@ app.post('/users', (req, res) => {
     .catch(e => {
       res.status(400).send(e)
     })
+})
+
+// Todos routes
+app.get('/todos', (req, res) => {
+  Todo.find().then(
+    todos => {
+      res.send({ todos })
+    },
+    e => {
+      res.status(400).send(e)
+    }
+  )
 })
 
 app.get('/todos/:id', (req, res) => {
